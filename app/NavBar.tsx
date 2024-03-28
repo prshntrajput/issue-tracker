e866d7993d896d99ea17ee24fd742c1a0c6d9859
Prashant Rajput <prshant.work@gmail.com>
2024-03-28 16:22:30 +0530

    setup radixui

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -16,7 +16,7 @@ const NavBar = () => {
         <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
             <Link href="/"><FaBug /></Link>
              <ul className='flex space-x-6'>
-                {links.map(link => <Link href={link.href} className={classnames({
+                {links.map(link => <Link key={link.href} href={link.href} className={classnames({
                     "text-zinc-900": link.href === currentPath,
                     "text-zinc-500": link.href !== currentPath,
                     "hover:text-zinc-800 font-semibold":true
